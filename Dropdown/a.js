7fdbef16aa1ed8d7bca52d9a5a79a336e7c2ef0c
YONGSU JEONG <jos50275266@gmail.com>
2020-12-09 00:13:02 +0900

    modification

--- Dropdown/a.js
+++ Dropdown/a.js
@@ -1,10 +1,26 @@
-const test = [
-    { name: 'a', value: 0},
-    { name: 'b', value: 1},
-    { name: 'c', value: 2},
-    { name: 'd', value: 3},
-    { name: 'e', value: 4}
-];
+// generate secret number is a random integer between 1 and 12
+const MIN = 1;
+const MAX = 12;
 
-console.log(test.some(e => e.name === 'c'));
-console.log(test.)
+let secretNumber = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
+
+let guesses = 0; // for storing the number of guesses
+let hint = ''; // for storing hint
+let number = 0;
+do {
+    // get input from user
+    let input = prompt(`Please enter a number between ${MIN} and ${MAX}` + hint);
+    // get the integer
+    number = parseInt(input);
+    // increase the number of guesses
+    guesses++;
+    // check input number with the secret number
+    // provide hint if needed
+    if (number > secretNumber) {
+        hint = ', and less than ' + number;
+    } else if (number < secretNumber) {
+        hint = ', and greater than ' + number;
+    } else if(number == secretNumber) {
+        alert(`Bravo! you're correct after ${guesses} guess(es).`);
+    }
+} while (number != secretNumber);
